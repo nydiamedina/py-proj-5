27d3b4d960fab3c073a01a7704821de17916307a
Nydia Medina <nvmedina29@gmail.com>
2023-01-05 05:29:07 -0600

    Add all_movies template and route to get them

--- starter/crud.py
+++ starter/crud.py
@@ -25,6 +25,12 @@ def create_movie(title, overview, release_date, poster_path):
     return movie
 
 
+def get_movies():
+    """Return all movies."""
+
+    return Movie.query.all()
+
+
 def create_rating(user, movie, score):
     """Create and return a new movie rating."""
 
